a5819b3a624fed2c8f692725511869c3be1e80b4
sarasarto <48065967+sarasarto@users.noreply.github.com>
2020-03-21 14:38:06 +0100

    - dopo login torna a homepage - sposato file login.html in registration

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from . import views
+
 from django.contrib import admin
 from django.conf.urls import url
 from django.contrib.auth.views import LoginView, LogoutView
@@ -11,6 +12,6 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
 
     # /home/login
-    url(r'login/$', LoginView.as_view(template_name='users/login.html'), name='login'),
+
     url(r'logout/$', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
--- tesi/urls.py
+++ tesi/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView
 from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls')),
     path('register/', user_views.register, name='register'),
+
+    path('login/', LoginView.as_view(), name='login'),
+    path('', include('home.urls')),
 ]
